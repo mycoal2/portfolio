417d7d0bf363f889ffd817614b8c735b4f8d3775
mycoal2 <michaelhong136@gmail.com>
2025-01-20 00:14:15 -0500

    update

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
   return (
     <section className="bg-primary px-4 text-white py-12" id="experience">
       <h3 className="project-title my-5"> Experience </h3>
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-mandatory snap-x">
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-mandatory snap-x items-center justify-center">
             <article className="bg-presagis flex flex-col rounded-lg items-center space-y-5 flex-shrink-0 snap-center overflow-hidden opacity-100">
                <div className="px-0 md:px-10 unselectable">
                   <h4 className="cursive overflow-hidden text-4xl text-blue-500">Software Developer</h4>
@@ -19,7 +19,23 @@ const Experience = () => {
                   </ul>
                </div>
             </article>
-      </div>      
+            <article className="bg-genetec flex flex-col rounded-lg items-center space-y-5 flex-shrink-0 snap-center overflow-hidden opacity-100">
+               <div className="px-0 md:px-10 unselectable z-10">
+                  <h4 className="cursive overflow-hidden text-4xl text-blue-500">Software Tester</h4>
+                  <p className="font-bold text-blue-400 text-3xl mt-1">Genetec</p>
+                  <div>
+                    <img src={process.env.PUBLIC_URL + '/Images/csharp.png'} alt="C#" className="rounded-lg w-6 h-6 float-left "/>
+                    <img src={process.env.PUBLIC_URL + '/Images/powershell.png'} alt="Powershell" className="rounded-lg w-6 h-6 float-left"/>
+                  </div>
+                  <p className="py-3 text-blue-200 ">Jan 2024 - Aug 2024</p>
+                  <ul className="space-y-3 ml-5 text-sm">
+                     <li className="mr-2">Developed a test cases and unit tests in C++</li>
+                     <li className="mr-2">User Acceptance Tester, document test results and report defects.</li>
+                     <li className="mr-2">Applied Agile and Scrum methodologies for project management.</li>
+                  </ul>
+               </div>
+            </article>
+      </div>     
    </section>
   )
 }
